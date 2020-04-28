318bb666509b806ccd847b858eaefdc4de08052b
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-28 15:27:57 +1000

    added test when login failed, added static page for home

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/login',    to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
+  get 'home', to: 'static_pages#home'
   resources :users
 end

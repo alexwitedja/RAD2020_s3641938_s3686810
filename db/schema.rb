1d1e3beb7e41e37a9074a5b93ed2a17097e4e15c
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-28 14:40:24 +1000

    added back name column because I realise we need it later on, but it's not used at the moment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_042453) do
+ActiveRecord::Schema.define(version: 2020_04_28_043856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_042453) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "mobile"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

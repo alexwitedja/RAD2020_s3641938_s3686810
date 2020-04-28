e732620bd990981c2aa57b91c67e97840426e23b
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-28 16:51:03 +1000

    finish login upon signup, logout, and login now redirects to static_pages#home

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,14 +6,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:alex)
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
-    assert_redirected_to @user
+    assert is_logged_in?
+    assert_redirected_to home_path
     follow_redirect!
-    assert_template 'users/show'
+    assert_template 'static_pages/home'
     assert_select "a[href=?]", login_path, count:0
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
   end
 
   test "login with invalid information" do

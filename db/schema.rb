d888be0a930526982f7a4b3c06ccc27a88acf196
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-29 12:51:27 +1000

    added remember_digest column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_043856) do
+ActiveRecord::Schema.define(version: 2020_04_29_025050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_043856) do
     t.string "password_digest"
     t.string "mobile"
     t.string "name"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

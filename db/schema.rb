bd23a87f555ea5d98313b440b0659dfed9c60d07
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-30 13:10:04 +1000

    added posts table, user_id is the foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_025050) do
+ActiveRecord::Schema.define(version: 2020_04_30_030857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "topic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,8 +31,10 @@ ActiveRecord::Schema.define(version: 2020_04_29_025050) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "mobile"
+    t.string "name"
     t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end

3aa24d48449153279d4666c73cd92a25ad25f7ba
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-30 15:21:01 +1000

    added _post partial

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get '/login',    to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
-  get 'home', to: 'posts#index'
+  get '/home', to: 'posts#index'
   resources :users
 end

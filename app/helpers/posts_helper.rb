847f77e57cce056e6a4d08e74b8291b9aa1a6c70
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-30 21:15:46 +1000

    finished homepage, added helper functions in users and posts to get relevant data to render

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,15 @@
 module PostsHelper
-  # Get all posts in one month
-  def get_posts_one_month
-    @posts = Post.where(:created_at => 1.month.ago..Time.now)
+  def get_topics
+    @topics = ["A.I", "Game", "V.R", "REST"]
+  end
+  
+  # Get recent posts in this month
+  def get_recent_posts
+    Post.where(:created_at => 1.month.ago..Time.now)
+  end
+
+  # Get 5 most viewed posts, to be implemented
+  def get_popular_posts
+    Post.all
   end
 end

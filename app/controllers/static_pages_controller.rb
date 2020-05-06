0dbd383ff2bc7bdb2fc23276fa5c0b0145dc5332
alexwitedja <s3641938@student.rmit.edu.au>
2020-05-06 21:32:03 +1000

    home is now a layout, added selected and recent route

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,17 @@
 class StaticPagesController < ApplicationController
-  def home
+  def recent
     vars = request.query_parameters
     @posts = Post.where(:created_at => 1.month.ago..Time.now)
     if !vars.empty? # If true, user is going to selected posts.
       @posts = Post.first
     end
+  end
+
+  def selected
+    @posts = Post.first
+  end
+
+  def select
+    @topic = params[:topic]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get '/login',    to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
-  get '/home', to: 'static_pages#home'
+  get '/home', to: 'static_pages#recent'
+  get '/selected', to: 'static_pages#selected'
   resources :users
   resources :posts
 end

e0a21ce2535b1123a61d6373d52ba052d3dc2f03
alexwitedja <s3641938@student.rmit.edu.au>
2020-05-06 19:55:24 +1000

    get posts now in home controller, added link_to for buttons

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    vars = request.query_parameters
+    @posts = Post.where(:created_at => 1.month.ago..Time.now)
+    if !vars.empty? # If true, user is going to selected posts.
+      @posts = Post.first
+    end
   end
 end

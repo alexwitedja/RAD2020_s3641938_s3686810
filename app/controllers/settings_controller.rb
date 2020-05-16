a642612e229d41e9edae5625fb6d018e9a8641b2
davidd11 <ddoan.0411@gmail.com>
2020-05-16 17:47:35 +1000

    implement my post and my comments under settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,8 +1,17 @@
 class SettingsController < ApplicationController
+  include UsersHelper
+
   def profile
     @user = current_user
   end
 
-  def show
+  def posts
+    @posts = get_posts_by_user(current_user.id)
   end
+
+  def comments 
+    @comments = get_comments_by_user(current_user.id)
+  end
+
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,18 @@
 module UsersHelper
 
-    # Returns the Gravatar for the given user.
-    def gravatar_for(user, options = { size: 80 })
-      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-      size = options[:size]
-      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-      image_tag(gravatar_url, alt: user.email, class: "gravatar")
-    end
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.email, class: "gravatar")
+  end
 
-    # Returns recently active members.
-    def get_recent_members
-      # To be implemented
-      @users = User.all
-    end
+  # Returns recently active members.
+  def get_recent_members
+    # To be implemented
+    @users = User.all
+  end
     
   def get_posts_by_user(user_id)
     Post.where(:user_id => user_id)
@@ -22,4 +22,4 @@ module UsersHelper
     Comment.where(:user_id => user_id)
   end
 
-  end
+end

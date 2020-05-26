547c7a8e77f9b282069e311fdb504c2a2634ca7d
alexwitedja <s3641938@student.rmit.edu.au>
2020-05-26 16:37:40 +1000

    test hotfix

--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class SettingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get setting" do
-    get settings_setting_url
-    assert_response :success
-  end
-
+  
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(email: "user@example.com", mobile: "1234567890",
-                      password: "foobar", password_confirmation: "foobar")
+    @user = users(:alex)
     @post = @user.posts.new(topic: "A.I", title: "A.I is very cool!",
                               content: "blah blah A.I blah blah")
   end

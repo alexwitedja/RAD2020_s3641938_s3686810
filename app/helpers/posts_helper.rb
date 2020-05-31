bc0caffe50f28452f976c9d2217fbaf43487455b
davidd11 <ddoan.0411@gmail.com>
2020-05-31 15:07:36 +1000

    news dropdown in header implemented

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,6 +2,29 @@ module PostsHelper
   def get_topics
     @topics = ["A.I", "Game", "V.R", "Rest"]
   end
+
+  def get_popular_topics
+    topic_list = ["A.I", "Game", "V.R", "Rest"]
+    view_counts = []
+    topic_list.each do |topic|
+      views = 0
+      Post.where(:topic => topic).each do |post|
+        views += post.view_count
+      end
+      view_counts.push(views)
+    end
+
+    @popular_topics = ["News"]
+
+    largest_index = view_counts.index(view_counts.max)
+    @popular_topics.push(topic_list[largest_index])
+    topic_list.delete_at(largest_index)
+    view_counts.delete_at(largest_index)
+    second_largest_index = view_counts.index(view_counts.max)
+    @popular_topics.push(topic_list[second_largest_index])
+
+    @popular_topics
+  end
   
   # Get recent posts in this month
   def get_recent_posts

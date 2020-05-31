0493581f1d1dfabe4aa24f653a143047facec667
alexwitedja <s3641938@student.rmit.edu.au>
2020-05-31 15:44:04 +1000

    deploy to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'alex-david-a1.herokuapp.com'
+  host = 'david-alex-a1.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
